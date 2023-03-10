09d57efe0a80092051b74f46437eb3f059d3053c
SH.CHOI <92343445+choisangh@users.noreply.github.com>
2023-03-11 01:06:41 +0900

    REFACTOR: 코드 정리 및 주석 추가

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,6 +1,7 @@
 import itertools
 from keras.optimizers import Adam
 from .model import create_model, markowitz_objective
+from config import N_TIME
 
 
 param_grid = {'lstm_units': [16, 32, 64],
@@ -12,19 +13,35 @@ best_params = None
 best_val_loss = float('inf')
 
 
-def optimizer(N_TIME, N_STOCKS, xc_train, xf_train):
+def optimizer(xc_train, xf_train, xc_test, xf_test):
+    """
+    A function to optimize hyperparameters by training multiple models with different parameter combinations.
+
+    :param xc_train: Training input data (company features)
+    :param xf_train: Training output data (future stock returns)
+    :param xc_test: Testing input data (company features)
+    :param xf_test: Testing output data (future stock returns)
+    :return: None
+    """
+    # iterate over parameter combinations
     for params in param_combinations:
-        model = create_model(N_TIME, N_STOCKS)
+        # create a model with the current parameter combination
+        model = create_model(N_TIME, xc_train.shape[2])
 
-        model.compile(loss=markowitz_objective,
-                      optimizer=Adam(learning_rate=1e-5))
+        # compile the model with the custom loss function and Adam optimizer
+        model.compile(loss=markowitz_objective, optimizer=Adam(learning_rate=1e-5))
 
+        # fit the model to the training data and validate on the testing data
         hist = model.fit(xc_train, xf_train, epochs=1000, batch_size=params[1], validation_data=(xc_test, xf_test))
 
+        # get the validation loss of the trained model
         val_loss = hist.history['val_loss'][-1]
 
+        # if the current model has the best validation loss so far, save the model's parameters and validation loss
         if val_loss < best_val_loss:
             best_params = params
             best_val_loss = val_loss
+
+        # print the best parameters so far
         print('Best params:', best_params)
 
